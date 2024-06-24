d437f3b3c7bc4b8e66e8883e8d6b7d4dcb2b89f7
LutsenkoVitaliy <lutsenko996@gmail.com>
2024-06-24 15:18:20 +0300

    function file system

---
+++ services/contactsServices.js
@@ -0,0 +1,52 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { nanoid } from 'nanoid';
+
+const contactsPath = path.join(__dirname, '../db/contacts.json');
+
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data)
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === contactId);
+  return result || null
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) {
+    return null
+  };
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
+}
