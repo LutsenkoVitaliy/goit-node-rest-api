2daa9ee05c3da4a31b0db2d615b1eb9b59fad48f
LutsenkoVitaliy <lutsenko996@gmail.com>
2024-06-26 12:47:45 +0300

    add_Mango

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import contactsRouter from "./routes/contactsRouter.js";
+
+const DB_HOST = "mongodb+srv://lutsenko996:nLlkObqk10Wa1KOJ@atlascluster.zwxkrej.mongodb.net/"
+mongoose.connect()
 
 const app = express();
 
